2c8552bc53ae2fba8e57f45530c2861ca6b9267b
ChenZhaoLi <49938285+ChenZhaoLi@users.noreply.github.com>
2021-05-06 00:30:40 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    themeConfig: {
+        sidebar: [
+          '/',
+
+          {
+              title:'css',
+              children:[
+                ['/css/css权重', '权重']
+              ]
+          }
+         
+        ]
+      }
+  }
