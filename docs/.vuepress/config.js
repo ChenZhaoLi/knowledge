9a04b3b33a56e374d97112999c46c6dee10b40e6
pipizhao <1097199965@qq.com>
2021-08-09 14:54:24 +0800

    tt

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,13 @@
 module.exports = {
-    themeConfig: {
-        sidebar: [
-          '/',
+  base: '/knowledge/',
+  themeConfig: {
+    sidebar: [
+      '/',
 
-          {
-              title:'css',
-              children:[
-                ['/css/css权重', '权重']
-              ]
-          }
-         
-        ]
-      }
-  }
+      {
+        title: 'css',
+        children: [['/css/css权重', '权重']],
+      },
+    ],
+  },
+}
