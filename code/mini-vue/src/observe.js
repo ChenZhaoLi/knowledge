1475b64a9b22016ba520a66158d871f3f0b6eb20
pipizhao <1097199965@qq.com>
2021-08-09 09:52:35 +0800

    github action test

--- code/mini-vue/src/observe.js
+++ code/mini-vue/src/observe.js
@@ -1,18 +1,17 @@
-import Observer from "./observer.js"
+import Observer from './observer.js'
 /**
  * 通过 Observer 类为对象设置响应式能力
- * @param {*} value 
+ * @param {*} value
  * @returns Observer 实例
  */
-export default function observe(value){
-    // 避免无限递归
-    // 当 value 不是对象直接结束递归
-    if(typeof value !== 'object') return
+export default function observe(value) {
+  // 避免无限递归
+  // 当 value 不是对象直接结束递归
+  if (typeof value !== 'object') return
 
-    // value.__ob__ 是 Observer 实例
-    // 如果 value.__ob__属性已经存在，说明 value 对象已经具备响应式能力，直接返回已有的响应式对象
-    if(value.__ob__) return value.__ob__
-    
-    return new Observer(value)
+  // value.__ob__ 是 Observer 实例
+  // 如果 value.__ob__属性已经存在，说明 value 对象已经具备响应式能力，直接返回已有的响应式对象
+  if (value.__ob__) return value.__ob__
 
+  return new Observer(value)
 }
